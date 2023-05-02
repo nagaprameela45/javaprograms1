7c6f039318c2de729130dff9a66d6e9d9e07b194
nagaprameela45 <130998594+nagaprameela45@users.noreply.github.com>
2023-05-02 09:54:45 +0530

    Add files via upload

---
+++ dailyassignments/week2/day6/class BankDetials.java
@@ -0,0 +1,14 @@
+class BankDetials
+  {
+    public static void main(String args[])
+    {
+      String name="prameela";
+      long accnum=349648909l;
+      float accbal =18000f;
+      String  Ifsc="CBI10000000";
+      System.out.println("my name is "+ name);
+      System.out.println("my account number is  "+ accnum);
+      System.out.println("my account balance is  "+accbal);
+      System.out.println(" bank ifsc code is " + Ifsc);
+    }
+  }
---
+++ dailyassignments/week2/day6/class SelfDetails.java
@@ -0,0 +1,25 @@
+class SelfDetails
+  {
+    public static void main(String args[])
+    {
+      String name="prameela";
+      String address="Vijayawada";
+      char grade='B';
+      int marks=73;
+      double percentage=82.5;
+      long phnum=8688104086L;
+      boolean marital_status=false;
+      System.out.println("Your name is "+name);
+      System.out.println("Your address is "+address);
+      System.out.println(grade+ " is your grade" );
+      System.out.println("Your marks is "+marks);
+      System.out.println("Your percentage is "+percentage);
+      System.out.println("Your phone number is "+phnum);
+      System.out.println("Your marital status is "+marital_status);
+    }
+  }
+
+    
+    
+    
+  
